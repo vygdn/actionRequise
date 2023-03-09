fa092b77f4de69364ab7ef52a4723760d6e6e174
Valentine <105112125+vygdn@users.noreply.github.com>
2023-03-09 16:06:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function demandeAge() {
       alert("Bravo! Tu peux accéder à notre site de campagne!");
     } else {
       alert("Désolé, vous devez avoir au moins 18 ans pour accéder à notre site.");
-     // window.location.href = "https://www.google.com/"; // redirige l'utilisateur vers Google
+      window.location.href = "https://www.google.com/"; // redirige l'utilisateur vers Google
     }
   }
